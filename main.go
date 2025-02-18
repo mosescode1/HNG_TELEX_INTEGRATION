9f311b8bf12f14ff4b1d8968a061c5b2336fc676
mosescode1 <emosescode@gmail.com>
2025-02-18 18:18:53 +0100

    chore: Add logger and CORS middleware to the Fiber application

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/mosescode1/routes"
 )
 
 
 func main() {
 	app := fiber.New()
+
+	app.Use(logger.New())
+	app.Use(cors.New())
 
 	routes.SetupRoutes(app)
 
